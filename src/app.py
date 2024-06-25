3047861548bcadcfe5134a3be5cecaacaab121ed
pks957 <pks957@gmail.com>
2024-06-25 19:51:10 +0000

    a lot of changes

--- src/app.py
+++ src/app.py
@@ -16,7 +16,15 @@ def before_request():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+    
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/templetes')
+def templates():
+    return render_template('elements.html')
 
 @app.route('/<sign_id>')
 def show_sign(sign_id):
@@ -30,8 +38,20 @@ def show_sign(sign_id):
 def create_sign():
     if request.method == 'POST':
         sign_id = str(uuid.uuid4())
-        new_sign = Sign(id=sign_id, business_name=request.form['business_name'], hours=request.form['hours'])
+        
+        new_sign = Sign(
+                    id=sign_id, 
+                    opening_time=request.form['opening-time'], 
+                    closing_time=request.form['closing-time'],
+                    is_open_monday = True if request.form.get('od-monday', False) == 'on' else False,
+                    is_open_tuesday = True if request.form.get('od-tuesday', False) == 'on' else False,
+                    is_open_wednesday = True if request.form.get('od-wednesday', False) == 'on' else False,
+                    is_open_thursday = True if request.form.get('od-thursday', False) == 'on' else False,
+                    is_open_friday = True if request.form.get('od-friday', False) == 'on' else False,
+                    is_open_saturday = True if request.form.get('od-saturday', False) == 'on' else False,
+                    is_open_sunday = True if request.form.get('od-sunday', False) == 'on' else False
+                    )
         db.session.add(new_sign)
         db.session.commit()
         return redirect(url_for('show_sign', sign_id=sign_id))
-    return render_template('create.html')
+    return render_template('create_sign.html')
--- src/models.py
+++ src/models.py
@@ -4,5 +4,13 @@ db = SQLAlchemy()
 
 class Sign(db.Model):
     id = db.Column(db.String, primary_key=True)
-    business_name = db.Column(db.String(80), nullable=False)
-    hours = db.Column(db.String(120), nullable=False)
+    opening_time = db.Column(db.String(30), nullable=False)
+    closing_time = db.Column(db.String(30), nullable=False)
+    is_open_monday = db.Column(db.Boolean, nullable=False)
+    is_open_tuesday = db.Column(db.Boolean, nullable=False)
+    is_open_wednesday = db.Column(db.Boolean, nullable=False)
+    is_open_thursday = db.Column(db.Boolean, nullable=False)
+    is_open_friday = db.Column(db.Boolean, nullable=False)
+    is_open_saturday = db.Column(db.Boolean, nullable=False)
+    is_open_sunday = db.Column(db.Boolean, nullable=False)
+
